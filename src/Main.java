73994119ce0b68fa354ef2ecd6c08260fb8e24bc
Admin <mr.mr.sakolov@mail.ru>
2024-10-28 12:31:09 +0300

    Домашняя работа Объекты и классы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашняя работа Объекты и классы");
+        Author author = new Author("Александр", "Пушкин");
+        Author author1 = new Author("Лев", "Толстой");
+        Book book = new Book("Капитанская дочка",author,1836);
+        Book book1 = new Book("Война и мир",author1,1863);
+        book1.setYearBook(2024);
+        System.out.println("Название книги: " + book.getNameBook() + " Автор: " + author.getName()+ " " + author.getSurname()+" Год публикации: "+ book.getYearBook());
+        System.out.println("Название книги: " + book1.getNameBook() + " Автор: " + author1.getName()+ " " + author1.getSurname()+" Год публикации: "+ book1.getYearBook());
     }
 }
